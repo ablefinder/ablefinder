00a2b8ecf055c82dbf74c37730d211a879b8d95d
Shrijan Regmi <ilyyhs9@gmail.com>
2020-08-17 10:02:57 +0545

    Text changes

--- src/js/main.js
+++ src/js/main.js
@@ -355,3 +355,15 @@ $(document).ready(function() {
       });
   })(jQuery);
 }); 
+
+// get website visitor's location
+$.getJSON('https://ipapi.co/json/', function (data) {
+  console.log(data);
+  var country = data.country_name;
+  var headerLogo = $('.afford_with_text');
+  var footerLogo = $('.powered')
+  if(country !== 'Australia'){
+    headerLogo.css('display', 'none');
+    footerLogo.css('display', 'none');
+  }
+});
